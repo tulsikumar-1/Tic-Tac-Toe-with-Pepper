f63882440fa1fc9c9d2b0f34d154f6594cad67d7
Tulsi Kumar <104934568+tulsikumar-1@users.noreply.github.com>
2023-08-12 22:27:30 +0200

    Add files via upload

---
+++ Behaviours/comecloser/translations/translation_en_US.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Please come closer</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Please come closer</translation>
+        </message>
+    </context>
+</TS>
